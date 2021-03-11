582dd4e9baa1b466b427fbaa586c3f6c493d034b
Conrad, Ashley <Conrad.Ashley@principal.com>
2021-03-11 14:22:14 -0600

    Week two exercise, part part three. Test 4, 5 , 6 success.

--- src/test/java/WeekTwoExercisePartThree.java
+++ src/test/java/WeekTwoExercisePartThree.java
@@ -60,10 +60,31 @@ public class WeekTwoExercisePartThree {
     }
 
     @Test
-    @Disabled
     public void sendingTwentySixteenReturnsOnlyOneFridayTheThirteenth() {
         // TODO: Implement the code to make this test pass
         // TODO: don't forget to commit after passing the test
+        LocalDate[] actual = unluckyDatesByYear(2016);
+        LocalDate[] expected = new LocalDate[]{
+                LocalDate.of(2016, 5, 13),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sendingTwentyNineteenReturnsTwoFridayTheThirteenths() {
+        //  TODO: write this test and, if necessary, make any changes to make it pass
+        //  TODO: don't forget to commit after passing the test
         LocalDate[] actual = unluckyDatesByYear(2019);
         LocalDate[] expected = new LocalDate[]{
                 LocalDate.of(2019, 9, 13),
@@ -83,35 +104,46 @@ public class WeekTwoExercisePartThree {
     }
 
     @Test
-    @Disabled
-    public void sendingTwentyNineteenReturnsTwoFridayTheThirteenths() {
-        //  TODO: write this test and, if necessary, make any changes to make it pass
-        //  TODO: don't forget to commit after passing the test
-    }
-
-    @Test
-    @Disabled
     public void sendingTwentyFifteenReturnsThreeFridayTheThirteenths() {
         //  TODO: write this test and, if necessary, make any changes to make it pass
         //  TODO: don't forget to commit after passing the test
+        LocalDate[] actual = unluckyDatesByYear(2015);
+        LocalDate[] expected = new LocalDate[]{
+                LocalDate.of(2015, 2, 13),
+                LocalDate.of(2015, 3, 13),
+                LocalDate.of(2015, 11, 13),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null};
+
+        assertArrayEquals(expected, actual);
     }
 
     // TODO Implementation Implement your changes to make the tests pass here...
 
     public LocalDate[] unluckyDatesByYear(int year) {
         LocalDate[] localDates = new LocalDate[12];
-//        int i = 0;
-//        for (int m = 1; m < 13; m++){
-//            if
-//        }
+        int i = 0;
+        for (int mm = 1; mm < 13; mm++){
+            if (isUnluckyDate(year, mm, 13)) {
+                LocalDate thirteen = LocalDate.of(year, mm, 13);
+                localDates[i++] = thirteen;
+            }
+        }
         return localDates;
     }
 
     public boolean isUnluckyDate(int year, int month, int day) {
         if (day == 13) {
-//            LocalDate thisDay = LocalDate.of(year, month, day);
-//            if(thisDay.getDayOfWeek() == DayOfWeek.FRIDAY)
-            return true;
+            LocalDate thisDay = LocalDate.of(year, month, day);
+            if(thisDay.getDayOfWeek() == DayOfWeek.FRIDAY)
+                return true;
         }
         return false;
     }
